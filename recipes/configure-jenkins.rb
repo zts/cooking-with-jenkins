f6b4c6382b8614340d7e04de9f323f04dc9b5efd
Zachary Stevens <zts@cryptocracy.com>
2013-12-27 19:23:06 +0000

    Add recipe to configure Jenkins before creating jobs.

---
+++ recipes/configure-jenkins.rb
@@ -0,0 +1,36 @@
+#
+# Cookbook Name:: cooking-with-jenkins
+# Recipe:: configure-jenkins
+#
+# Adds plugins and common configuration we'll rely on in our Jenkins
+# job definitions.
+#
+# Copyright (C) 2013 Zachary Stevens
+# 
+# All rights reserved - Do Not Redistribute
+#
+
+# These plugins allow us to pull code (for jobs) from git
+jenkins_plugin "scm-api"
+jenkins_plugin "git-client"
+jenkins_plugin "git"
+
+# This colourises console output, instead of displaying literal ansi
+# escape sequences in the browser.
+jenkins_plugin "ansicolor"
+
+# This plugin lets us define static files which can (optionally) be
+# installed into a job's workspace before running any commands.  We
+# use it to override the test-kitchen configuration to use docker
+# instead of vagrant.
+cookbook_file "#{node[:jenkins][:server][:home]}/custom-config-files.xml" do
+  owner "jenkins"
+  group "jenkins"
+  mode "0644"
+  notifies :restart, "service[jenkins]"
+end
+jenkins_plugin "token-macro"
+jenkins_plugin "config-file-provider" do
+  version "2.7"
+  action :install
+end
--- recipes/default.rb
+++ recipes/default.rb
@@ -13,26 +13,8 @@ include_recipe "cooking-with-jenkins::install"
 # Prepare docker for use under jenkins
 include_recipe "cooking-with-jenkins::configure-docker"
 
-# Install git, and related Jenkins plugins
-jenkins_plugin "scm-api"
-jenkins_plugin "git-client"
-jenkins_plugin "git"
-
-## Stuff for test-kitchen
-jenkins_plugin "ansicolor" # colourise console output
-
-# provide additional static files to jobs
-cookbook_file "#{node[:jenkins][:server][:home]}/custom-config-files.xml" do
-  owner "jenkins"
-  group "jenkins"
-  mode "0644"
-  notifies :restart, "service[jenkins]"
-end
-jenkins_plugin "token-macro"
-jenkins_plugin "config-file-provider" do
-  version "2.7"
-  action :install
-end
+# Prepare jenkins for running jobs
+include_recipe "cooking-with-jenkins::configure-jenkins"
 
 # Add Jenkins job for a repository
 repo = "https://github.com/zts/chef-cookbook-managed_directory.git"
