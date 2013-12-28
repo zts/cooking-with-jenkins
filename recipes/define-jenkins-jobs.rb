63adda82faa649575610ea9895228bdbe7274bcb
Zachary Stevens <zts@cryptocracy.com>
2013-12-28 17:19:41 +0000

    Parameterise jenkins job build command.

--- recipes/define-jenkins-jobs.rb
+++ recipes/define-jenkins-jobs.rb
@@ -19,9 +19,15 @@ jenkins_job job_name do
   config job_config
 end
 
+build_command = <<-EOF
+bundle install --path .vendor --without integration
+bundle exec rake lint
+bundle exec rake spec
+bundle exec rake kitchen:all
+EOF
 template job_config do
   source 'cookbook-job.xml.erb'
-  variables :git_url => repo, :git_branch => 'master'
+  variables :git_url => repo, :git_branch => 'master', :command => build_command
   notifies  :update, "jenkins_job[#{job_name}]", :immediately
   notifies  :build, "jenkins_job[#{job_name}]", :immediately
 end
