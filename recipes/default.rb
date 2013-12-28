3655a229a0da152618b354938477f3592ab9bad1
Zachary Stevens <zts@cryptocracy.com>
2013-12-28 15:25:34 +0000

    Add recipe to define cookbook testing jobs in Jenkins.

--- recipes/default.rb
+++ recipes/default.rb
@@ -16,19 +16,5 @@ include_recipe "cooking-with-jenkins::configure-docker"
 # Prepare jenkins for running jobs
 include_recipe "cooking-with-jenkins::configure-jenkins"
 
-# Add Jenkins job for a repository
-repo = "https://github.com/zts/chef-cookbook-managed_directory.git"
-job_name = "cookbook-managed_directory"
-job_config = File.join(node[:jenkins][:server][:home], "#{job_name}-config.xml")
-
-jenkins_job job_name do
-  action :nothing
-  config job_config
-end
-
-template job_config do
-  source 'cookbook-job.xml.erb'
-  variables :git_url => repo, :git_branch => 'master'
-  notifies  :update, "jenkins_job[#{job_name}]", :immediately
-  notifies  :build, "jenkins_job[#{job_name}]", :immediately
-end
+# Create jobs for the cookbooks we're testing
+include_recipe "cooking-with-jenkins::define-jenkins-jobs"
