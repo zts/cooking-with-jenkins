5201862dee10bbed2aaf4b974daf29d2ee57d3f8
Zachary Stevens <zts@cryptocracy.com>
2013-12-29 00:48:39 +0000

    Update license.

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'cooking-with-jenkins'
 maintainer       'Zachary Stevens'
 maintainer_email 'zts@cryptocracy.com'
-license          'All rights reserved'
+license          'Apache 2.0'
 description      'Cookbook to demonstrate using Jenkins for Chef Cookbook CI'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
